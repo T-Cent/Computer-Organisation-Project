ee1a714f947776d918410e8537297a0e5eb5b8c0
Taraash <110958862+T-Cent@users.noreply.github.com>
2024-03-11 22:40:39 +0530

    Added some more decorative output

--- parser.py
+++ parser.py
@@ -1,3 +1,4 @@
+from cProfile import label
 import re
 from lookup import R_Type, I_Type, S_Type, B_Type, U_Type, J_Type, Registers
 import suggester as sg
@@ -23,19 +24,71 @@ if __name__ == "__main__":
     output_file_bin = open("output_bin.txt", "w")
 
     binary_output_list = []
+    labels_pointer = {}
+    stack_pointer = 0
 
     c = sg.colors()
     
     global_correct = True
+
+    def twos_complement(bits, number):
+        if number >= 0:
+            return bin(number)
+        else:
+            x = bin(abs(number))
+            x = "0"*(bits+2-len(x)) + x[2:]
+            y = "1"*bits
+            x = int(x, base=2)^int(y, base=2)
+            x += 0b1
+            return bin(x)
+      
+    def check_registers(line, *reg):
+        correct_f = True
+        for j in reg:
+            i = one_line_data[j]
+            if (i not in Unique_Registers):
+                correct_f = False
+                print(c.red(f"Error encountered at line {c.highlight_white(line+1)}, given register {c.highlight_red(i)} does not match any known register."))
+
+        if not correct_f:
+            global_correct = False
+            return True
+
+
+
+
+    #getting all labels and their corresponding pointers first
     for line, one_line in enumerate(data):
-        one_line = one_line[:-1] # removing the \n at the end
-        one_line_data = list(filter(lambda x: x, re.split(",| |\(|\)", one_line)))
+        one_line_data = list(filter(lambda x: x, re.split(",| |\(|\)|\t|\n", one_line)))
+        
+        stack_pointer += 4
+
+        #ignoring empty lines
+        if not len(one_line_data):
+            stack_pointer -= 4
+            continue
+
+        #checking for a label
+        if one_line_data[0][-1] == ":":
+            labels_pointer[one_line_data[0][:-1]] = stack_pointer
+            continue 
+
+
+    for line, one_line in enumerate(data):
+        one_line_data = list(filter(lambda x: x, re.split(",| |\(|\)|\t|\n", one_line)))
 
         binary_output = ''
         
         correct = True
 
-        if (one_line_data[0] in All_Instructions):
+
+        #checking for empty lines
+        if not len(one_line_data):
+            continue
+
+
+        temp_labels = list(map(lambda x: x+":", list(labels_pointer.keys())))
+        if (one_line_data[0] in (All_Instructions+temp_labels)):
             correct *= True
         else:
             correct *= False
@@ -45,25 +98,21 @@ if __name__ == "__main__":
             sg.suggestions_for(one_line_data[0])
         
 
-        only_registers = filter(lambda x: x[0] in "abcdefghijklmnopqrstuvwxyz", one_line_data[1:])
-        
-        for i in only_registers:
-            if (i not in Unique_Registers):
-                correct *= False
-                print(c.red(f"Error encountered at line {c.highlight_white(line+1)}, given register {c.highlight_white(i)} does not match any know register."))
+        global_correct *= correct
 
         if not correct:
             continue
 
-        #checking for a label
-        if one_line_data[0][-1] == ":": 
-            continue 
+
 
         if (one_line_data[0] in R_Type_Instructions):
+            if check_registers(line, 1, 2, 3):
+                continue
             binary_output += R_Type[one_line_data[0]]["opcode"] + Registers[one_line_data[1]] + R_Type[one_line_data[0]]["funct3"] + Registers[one_line_data[2]] + Registers[one_line_data[3]] + R_Type[one_line_data[0]]["funct7"]
 
         elif (one_line_data[0] in I_Type_Instructions):
             if one_line_data[0] == "lw":
+                check_registers(line, 1, 3)
                 if (x:=int(one_line_data[2])) > (max:=2**12 - 1):
                     print(c.red_a,f"Immediate value is larger than allowed, maximum value is {c.highlight_white(max)}")
                     continue
@@ -73,6 +122,8 @@ if __name__ == "__main__":
                     binary_output += I_Type[one_line_data[0]]["opcode"] + Registers[one_line_data[1]] + I_Type[one_line_data[0]]["funct3"] + Registers[one_line_data[3]] + one_line_data[2]
 
             else:
+                if check_registers(line, 1, 2):
+                    continue
                 if (x:=int(one_line_data[3])) > (max:=2**12 - 1):
                     print(c.red_a,f"Immediate value is larger than allowed, maximum value is {c.highlight_white(max)}")
                     continue
@@ -83,6 +134,8 @@ if __name__ == "__main__":
       
                     
         elif (one_line_data[0] in S_Type_Instructions):
+                if check_registers(line, 1, 3):
+                    continue
                 if (x:=int(one_line_data[2])) > (max:=2**12 - 1):
                     print(c.red_a,f"Immediate value is larger than allowed, maximum value is {c.highlight_white(max)}")
                     continue
@@ -91,16 +144,30 @@ if __name__ == "__main__":
           
                     binary_output += S_Type["sw"]["opcode"] + one_line_data[2][0:4] + S_Type["sw"]["funct3"] + Registers[one_line_data[1]] + Registers[one_line_data[3]] + one_line_data[5:11]
 
+
         elif (one_line_data[0] in B_Type_Instructions):
+            if check_registers(line, 1, 2):
+                continue
+            try:
                 if (x:=int(one_line_data[3])) > (max:=2**12 - 1):
                     print(c.red_a,f"Immediate value is larger than allowed, maximum value is {c.highlight_white(max)}")
                     continue
+
+            except ValueError:
+                if (x:=one_line_data[3] in labels_pointer.keys()):
+                    x = twos_complement(12, 4*line-labels_pointer[one_line_data[3]])
                 else:
-                    one_line_data[3] = '0'*(14-len(bin(x)))+bin(x)[2:]
+                    global_correct = False
+                    sg.suggestions_for(x, list(labels_pointer.keys()), "label")
+
+                                        
+            one_line_data[3] = '0'*(14-len(x))+x[2:]
           
-                    binary_output += B_Type[one_line_data[0]]["opcode"] + one_line_data[3][10] + one_line_data[3][0:4] + B_Type[one_line_data[0]]["funct3"] + Registers[one_line_data[1]] + Registers[one_line_data[2]] + one_line_data[3][4:10] + one_line_data[3][11]
+            binary_output += B_Type[one_line_data[0]]["opcode"] + one_line_data[3][10] + one_line_data[3][0:4] + B_Type[one_line_data[0]]["funct3"] + Registers[one_line_data[1]] + Registers[one_line_data[2]] + one_line_data[3][4:10] + one_line_data[3][11]
 
         elif (one_line_data[0] in U_Type_Instructions):
+                if check_registers(line, 1):
+                    continue
                 if (x:=int(one_line_data[2])) > (max:=2**20 - 1):
                     print(c.red_a,f"Immediate value is larger than allowed, maximum value is {c.highlight_white(max)}")
                     continue
@@ -110,22 +177,38 @@ if __name__ == "__main__":
                     binary_output += U_Type[one_line_data[0]]["opcode"] + Registers[one_line_data[1]] + one_line_data[2]
 
         elif (one_line_data[0] in J_Type_Instructions):
+            if check_registers(line, 1):
+                continue
+            try:
                 if (x:=int(one_line_data[2])) > (max:=2**20 - 1):
                     print(c.red_a,f"Immediate value is larger than allowed, maximum value is {c.highlight_white(max)}")
                     continue
-                else:
-                    one_line_data[2] = '0'*(22-len(bin(x)))+bin(x)[2:]
 
-                    binary_output += J_Type["jal"]["opcode"] + Registers[one_line_data[1]] + one_line_data[2][11:19] + one_line_data[2][10] + one_line_data[2][0:9] + one_line_data[2][19]
+            except ValueError:
+                if ((x:=one_line_data[2]) in labels_pointer.keys()):
+                    print(x)
+                    x = twos_complement(20, 4*line-labels_pointer[x])
+                else:
+                    global_correct = False
+                    sg.suggestions_for(x, list(labels_pointer.keys()), "label")
+    
+            try:
+                int(one_line_data[2])
+                one_line_data[2] = '0'*(22-len(bin(x)))+bin(x)[2:]
+            except ValueError:
+                one_line_data[2] = '0'*(22-len(x))+x[2:]
+
+            binary_output += J_Type["jal"]["opcode"] + Registers[one_line_data[1]] + one_line_data[2][11:19] + one_line_data[2][10] + one_line_data[2][0:9] + one_line_data[2][19]
 
         binary_output_list.append(binary_output)
-        global_correct *= correct
+
 
     
     if global_correct:
         print("\x1b[32mNo errors detected, binary succesfully created.\x1b[0m")
         for x in binary_output_list:
-            output_file_bin.write(x)
-            output_file_bin.write("\n")
+            if len(x) != 0:
+                output_file_bin.write(x)
+                output_file_bin.write("\n")
         
     output_file_bin.close()
